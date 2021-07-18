1e3e4208386275e6153b2ccb1767f08b259697da
Yvan Pearson <yvan.pearson@gmail.com>
2021-07-18 12:15:50 -0400

    moving on to next section

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+'tim':{'age':19, 'gender':'male'},
+'john':{'age':29, 'gender':'male'}}
+
 class HelloWorld(Resource):
-    def get(self, myParam, number):
-        return {"data":"Hello World " + myParam + str(number)}
+    def get(self, name):
+        return names[name]
 
     def post(self):
         return {"response":"this is a post"}
 
 # api.add_resource(HelloWorld, "/api")
-api.add_resource(HelloWorld, "/api/<string:myParam>/<int:number>")
+api.add_resource(HelloWorld, "/api/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+
+# https://www.youtube.com/watch?v=GMppyAPbLYk
--- tests.py
+++ tests.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000"
 
-response = requests.get(BASE+'/api/bob/99', verify=False)
+response = requests.get(BASE+'/api/john', verify=False)
 print(response.json())
