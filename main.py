fefd0ae9918538f63a15d12bc2469eb777e8a048
Yvan Pearson <yvan.pearson@gmail.com>
2021-08-07 14:17:24 -0400

    moving to db part, @ 48min

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request, abort
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+
+# 200 --> OK
+# 201 --> Created
+# 404 --> Not Found
+# 409 --> Conflict if resource already exists
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database'
+db = SQLAlchemy(app)
 
-names = {
-'tim':{'age':19, 'gender':'male'},
-'john':{'age':29, 'gender':'male'}}
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+video_post_args = reqparse.RequestParser()
+video_post_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_post_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_post_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
-    def post(self):
-        return {"response":"this is a post"}
+videos={}
 
-# api.add_resource(HelloWorld, "/api")
-api.add_resource(HelloWorld, "/api/<string:name>")
+def abort_if_video_doesnot_exist(video_id):
+    if video_id not in videos:
+        print("Not Found")
+        abort(404, "Not Found")
+
+def abort_if_video_exist(video_id):
+    if video_id in videos:
+        print("Conflict resource already exists")
+        abort(409, "Conflict resource already exists")
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_doesnot_exist(video_id)
+        return videos[video_id], 200
+
+    def post(self, video_id):
+        print(request.form)
+        args = video_post_args.parse_args()
+        abort_if_video_exist(video_id)
+        videos[video_id] = args
+        print("POST")
+        print(videos)
+        return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_if_video_doesnot_exist(video_id)
+        video = videos.pop(video_id, 'No Key found')
+        print("DELETE")
+        print(videos)
+        return '', 200
+
+api.add_resource(Video, "/api/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
 
 
-
+# https://www.restapitutorial.com/lessons/httpmethods.html
 # https://www.youtube.com/watch?v=GMppyAPbLYk
--- tests.py
+++ tests.py
@@ -2,5 +2,20 @@ import requests
 
 BASE = "http://127.0.0.1:5000"
 
-response = requests.get(BASE+'/api/john', verify=False)
-print(response.json())
+videos = [
+    {"likes": "15", "views": 100, "name": "Some Video 1"},
+    {"likes": "215", "views": 200, "name": "Some Video 2"},
+    {"likes": "43", "views": 130, "name": "Some Video 3"},
+    {"likes": "1245", "views": 500, "name": "Some Video 4"},
+    {"likes": "500", "views": 700, "name": "Some Video 5"}]
+
+for i in range(0, len(videos)):
+    response = requests.post(
+        BASE+'/api/video/'+str(i), data=videos[i])
+    print(response.status_code)
+    print(response.json())
+    
+    response = requests.delete(
+        BASE+'/api/video/'+str(i))
+    print(response.status_code)
+    # print(response.json())
